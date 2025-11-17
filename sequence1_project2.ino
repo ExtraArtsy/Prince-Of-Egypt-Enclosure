9ca10e84fb0031605d2f809e06d8d5f0d99922ff
Artsy / Nova <102003934+ExtraArtsy@users.noreply.github.com>
2025-11-17 12:44:29 -0600

    Fixed up sequence 1 coed

--- sequence1_project2.ino
+++ sequence1_project2.ino
@@ -1,36 +1,27 @@
-// Boop de boop I am an edit. smiley face
-
 const int switchPin = 3;
-const int ledPin = 13;
+const int ledPinRed = 13;
+const int ledPinGreen = 12;
 int switchState = 0;
-#define RED_PIN 9
-#define GREEN_PIN 10
-#define BLUE_PIN 11
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(RED_PIN, OUTPUT);
-  pinMode(GREEN_PIN, OUTPUT);
-  pinMode(BLUE_PIN, OUTPUT);
+  pinMode(ledPinRed, OUTPUT);
+  pinMode(ledPinGreen, OUTPUT);
   pinMode(switchPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  setColor(255, 165, 0);
-  delay(1000);
+  delay(50);
   switchState = digitalRead(switchPin);
   Serial.println(switchState);
   if(switchState == HIGH) {
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPinRed, HIGH);
+    digitalWrite(ledPinGreen, HIGH);
+    analogWrite(ledPinGreen, 135);
+    analogWrite(ledPinRed, 155);
   }
   else {
-    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPinRed, LOW);
+    digitalWrite(ledPinGreen, LOW);
   }
 
-}
-
-void setColor(int redValue, int greenValue, int blueValue) {
-  analogWrite(RED_PIN, redValue);
-  analogWrite(GREEN_PIN, greenValue);
-  analogWrite(BLUE_PIN, blueValue);
 }
