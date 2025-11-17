fff1229b1f8ede74e9e77f95bdc41e085f1f71b2
Artsy / Nova <102003934+ExtraArtsy@users.noreply.github.com>
2025-11-17 14:41:53 -0600

    Updated sequence 2 to actually function
    This poor sequence was left to rot for a while

--- sequence2_project2.ino
+++ sequence2_project2.ino
@@ -1,21 +1,35 @@
-int sensorVal;
-const int sensorPin = A0;
-const int ledPin = 13;
+#include <Servo.h>
+
+Servo plagueServo1;
+Servo plagueServo2;
+
+const int switchPin = 2;
+int plagueSwitchState = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  plagueServo1.attach(5);
+  plagueServo2.attach(6);
+
+  pinMode(switchPin, INPUT);
+
+  plagueServo1.write(0);
+  plagueServo2.write(0);
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  sensorVal = analogRead(sensorPin);
-  Serial.println(sensorVal);
-  if(sensorVal <= 300){
-    digitalWrite(ledPin, HIGH);
-  } else {
-    digitalWrite(ledPin, LOW);
+  plagueSwitchState = digitalRead(switchPin);
+  // Coded so that the servos activate when the switch is no longer being pressed down on.
+  if(plagueSwitchState == HIGH) {
+    plagueServo1.write(0);
+    plagueServo2.write(180);
   }
+  else {
+    plagueServo1.write(180);
+    plagueServo2.write(0);
+
+  }
+  delay(50);
 
 }
